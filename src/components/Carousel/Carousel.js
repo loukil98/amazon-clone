fe91afc576faaa7570b16e7a5294e4ff2d146c07
Loukil Ghassen <loukilghassen98@gmail.Com>
2021-04-23 22:20:32 +0200

    infinite first try

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -3,6 +3,7 @@ import {carouselImages} from "../../data/carouselImages";
 import './Carousel.css'
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import transitions from "@material-ui/core/styles/transitions";
 
 /*
 const Carousel = () => {
@@ -46,54 +47,129 @@ const Carousel = () => {
 
 export default Carousel;
 */
-
 class Carousel extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            current: 0,
+            previous: carouselImages.length - 1,
+            next: 1,
             x: 0
         }
     }
 
     render() {
-
+    const imageStyle = this.state.x === 0 ?
+        {transform: `translateX(${this.state.x}%)`}
+        :
+        {transform: `translateX(${this.state.x}%)` , transition : "0.5s"}
         return (
             <div className="carousel">
-                {
-                    carouselImages.map((item, index) => {
-                        return (
-                            <img className={"carousel__image"} key={index} src={item}
-                                 style={{transform: `translateX(${this.state.x}%)`}}/>
-                        );
-                    })
-                }
 
-                <ArrowBackIosIcon className="carousel__ArrowBackIosIcon"
-                                  style={this.state.x / 100 === 0 ? {display: "none"} : {}}
-                                  onClick={() => {
-                                      this.handlePrevClick()
-                                  }}/>
-                <ArrowForwardIosIcon className="carousel__ArrowForwardIosIcon"
-                                     style={this.state.x / -100 === carouselImages.length - 1 ? {display: "none"} : {}}
-                                     onClick={() => {
-                                         this.handleNextClick()
-                                     }}/>
+                <img className="carousel__image" style={imageStyle}
+                     src={carouselImages[this.state.previous]}/>
+                <img className="carousel__image" style={imageStyle}
+                     src={carouselImages[this.state.current]}/>
+                <img className="carousel__image" style={imageStyle}
+                     src={carouselImages[this.state.next]}/>
+                <ArrowBackIosIcon className="carousel__ArrowBackIosIcon" onClick={() => {
+                    this.handlePrevClick()
+                }}/>
+                <ArrowForwardIosIcon className="carousel__ArrowForwardIosIcon" onClick={() => {
+                    this.handleNextClick()
+                }}/>
             </div>
         );
     }
 
-    handlePrevClick() {
-        console.log(this.state.x / 100)
-        if (this.state.x !== 0) {
-            this.setState({x: this.state.x + 100})
+
+    nextImage = () => {
+        if (this.state.current === carouselImages.length - 1) {
+            this.setState({current: 0, previous: carouselImages.length - 1, next: 1}, () => console.log(this.state))
+
+        } else {
+            this.setState(prevState => ({
+                current: prevState.current + 1,
+                previous: prevState.current,
+                next: prevState.next + 1
+            }), () => console.log(this.state))
+
+        }
+
+    }
+
+    prevImage = () => {
+        if (this.state.current === 0) {
+
+            this.setState({
+                current: carouselImages.length - 1,
+                previous: carouselImages.length - 2,
+                next: 0
+            }, () => console.log(this.state))
+        } else {
+            this.setState(prevState => ({
+                current: prevState.current - 1,
+                 previous: prevState.current - 2,
+                next: prevState.next + 1
+            }), () => console.log(this.state))
+        }
+
+    }
+    handleNextClick = () => {
+        if (this.state.current === carouselImages.length-2) {
+            this.setState({x: -100}, () => {
+                setTimeout(() => this.setState({
+                        current: carouselImages.length - 1,
+                        previous: carouselImages.length - 2,
+                        next: 0
+                    }, () => {
+                        this.setState({x: 0},()=>{
+                            console.log(this.state)})
+
+                    }), 500
+                )
+            })
+        }
+        else {
+            this.setState({x: -100}, () => {
+                setTimeout(() => this.setState(prevState => ({
+                        current: prevState.next,
+                        previous: prevState.previous - 1,
+                        next: prevState.next+1
+                    }), () => this.setState({x: 0} , ()=>console.log(this.state)))
+                    , 500)
+            })
+
+        }
+    }
+    handlePrevClick = () => {
+       if (this.state.current === 1) {
+            this.setState({x: 100}, () => {
+                setTimeout(() => this.setState({
+                        current: 0,
+                        previous: carouselImages.length - 1,
+                        next: 1
+                    }, () => {
+                        this.setState({x: 0},()=>{
+                            console.log(this.state)})
+
+                    }), 500
+                )
+            })
+        }
+        else {
+            this.setState({x: 100}, () => {
+                setTimeout(() => this.setState(prevState => ({
+                        current: prevState.previous,
+                        previous: prevState.previous - 1,
+                        next: prevState.current
+                    }), () => this.setState({x: 0} , ()=>console.log(this.state)))
+                    , 500)
+            })
+
         }
     }
 
-    handleNextClick() {
-        if (this.state.x / -100 !== carouselImages.length - 1) {
-            this.setState({x: this.state.x - 100})
-        }
-    }
 };
 
 export default Carousel;
